25c51e6d8b0ccadd9a0b337bf7aea0db09e01332
eLeMenCy <dev@elemency.com>
2020-09-05 17:49:27 +0200

    Added the WithBoth (callback + broadcast) sample and reviewed the readme log text.

--- src/main/java/com/elemency/Midi4J/Examples/App.java
+++ src/main/java/com/elemency/Midi4J/Examples/App.java
@@ -10,28 +10,34 @@ public class App {
 
         System.out.println("\n-----------------\n| Midi4J readme |\n" +
                 "-----------------------------------------------------------------------------------------\n" +
-                "This sample app demonstrates 2 methods on how midi IN messages can be handled.\n" +
-                "Midi4J allows the reception, per devices, of midi IN messages either via a centralised\n" +
-                "broadcaster or via user callbacks, the latter being called directly by the native driver\n" +
-                "whilst the former is called by a callback automatically set within each MidiIn instance.\n" +
-                "Look at the code and decide which method is the most adapted to your app requirements.\n\n" +
+                "This sample app demonstrates 3 methods on how midi IN messages can be handled in your\n" +
+                "application.\n" +
+                "Midi4J allows the reception, per devices, of midi IN messages, either via a centralised\n" +
+                "broadcaster and/or via user implemented callback(s).\n\n" +
 
-                "To start the sample with one or the other methods, type the following arg in the gradle\n" +
+                "To start the sample with one or the other method, type the following arg in your gradle\n" +
                 "console or in the 'Arguments' field of Intellij [run] Configurations window:\n" +
                 "\trun --args=\"broadcast\" (as broadcast mode is the default, arg is not required).\n" +
-                "\trun --args=\"callback\"\n\n" +
+                "\trun --args=\"callback\"\n" +
+                "\trun --args=\"both\"\n\n" +
 
                 "Broadcaster method (default)\n" +
                 "\tHandles all midi incoming events from MidiIn sourcedevices thru a centralised\n" +
                 "\tbroadcaster/listener system.\n\n" +
 
                 "Callback method\n" +
-                "\tHandles all midi incoming events thru user callbacks\n" +
-                "\t(1 callback per MidiIn source device).\n\n" +
+                "\tHandles all midi incoming events thru user callbacks (one per MidiIn device)\n\n" +
 
-                "These 2 methods of course can be used together in the same app by constructing MidiIn\n" +
-                "devices with the correct 'userCallback' param. Set to false, the device will use\n" +
-                "broadcast mode, set to true, the opposite.\n" +
+                "As hinted earlier, these 2 main methods can be used together in the same app by\n" +
+                "constructing MidiIn devices with the correct 'userCallback' param.\n" +
+                "Set to false, the device will use broadcast mode, set to true, the opposite.\n\n" +
+
+                "In Depth:\n" +
+                "A Broadcaster, is called by native driver callback(s) automatically set within each\n" +
+                "MidiIn instance at construction time. All messages are received by our app via the\n" +
+                "implementation of the BroadcasterListener interface and its overriden method.\n\n" +
+                "Callback(s) is/are called directly by the native driver and need to be set\n" +
+                "(one per In device) in user's app.\n" +
                 "-----------------------------------------------------------------------------------------");
 
         switch (args.length > 0 ? args[0] : "") {
@@ -40,8 +46,18 @@ public class App {
                 System.out.println("| Method with User Callbacks |");
                 System.out.println("------------------------------\n");
 
-                final WithCallbacks awcb = new WithCallbacks();
-                awcb.init();
+                final WithCallbacks withCallback = new WithCallbacks();
+                withCallback.init();
+                break;
+            }
+
+            case "both": {
+                System.out.println("\n\n--------------------");
+                System.out.println("| Method with Both |");
+                System.out.println("--------------------\n");
+
+                final WithBoth withBoth = new WithBoth();
+                withBoth.init();
                 break;
             }
 
@@ -50,8 +66,8 @@ public class App {
                 System.out.println("| Method with Broadcaster |");
                 System.out.println("---------------------------\n");
 
-                final WithBroadcaster awbst = new WithBroadcaster();
-                awbst.init();
+                final WithBroadcaster withBroadcaster = new WithBroadcaster();
+                withBroadcaster.init();
                 break;
             }
         }
